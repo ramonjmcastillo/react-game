7077b8be5f267c18bbb68a7175393747ddd12b24
Josh Castillo <ramonjmcastillo@gmail.com>
2019-10-04 10:44:32 +0800

    Added and connect redux to our App component.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
 
 class App extends React.Component {
   render() {
-    return <div className="App"></div>;
+    return (
+      <div className="App">
+        <h1> {this.props.message}</h1>
+      </div>
+    );
   }
 }
 
-export default App;
+App.propTypes = {
+  message: PropTypes.string.isRequired
+};
+
+const mapStateToProps = state => ({
+  message: state.message
+});
+
+export default connect(mapStateToProps)(App);
---
+++ src/redux/reducers/index.js
@@ -0,0 +1,9 @@
+const initialState = {
+  message: "I love redux"
+};
+
+function reducer(state = initialState) {
+  return state;
+}
+
+export default reducer;
